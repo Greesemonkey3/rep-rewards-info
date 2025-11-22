685a3b87b73968f36b6888d2934e940b97015374
Timothy Griesemer <Greesemonkey3@gmail.com>
2025-11-22 15:04:03 -0500

    more vercel changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/login',
+        destination: 'https://rep-dashboard-tawny.vercel.app/login',
+      },
+      {
+        source: '/login/:path*',
+        destination: 'https://rep-dashboard-tawny.vercel.app/login/:path*',
+      },
+      {
+        source: '/dashboard',
+        destination: 'https://rep-dashboard-tawny.vercel.app/dashboard',
+      },
+      {
+        source: '/dashboard/:path*',
+        destination: 'https://rep-dashboard-tawny.vercel.app/dashboard/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
