6943f66cc539397654d6e76727b91dad92cae421
Timothy Griesemer <Greesemonkey3@gmail.com>
2025-11-22 14:05:48 -0500

    more masking

--- next.config.js
+++ next.config.js
@@ -1,25 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/login',
-        destination: 'https://rep-dashboard-tawny.vercel.app/login',
-        permanent: true,
-      },
-      {
-        source: '/dashboard',
-        destination: 'https://rep-dashboard-tawny.vercel.app/dashboard',
-        permanent: true,
-      },
-      {
-        source: '/dashboard/:path*',
-        destination: 'https://rep-dashboard-tawny.vercel.app/dashboard/:path*',
-        permanent: true,
-      },
-    ]
-  },
 }
 
 module.exports = nextConfig
